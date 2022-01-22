133303d9fde0127189f1f886d6210a669f54ac14
Your Name <you@example.com>
2022-01-23 00:22:13 +0800

    ver 1

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, redirect, session, render_template, request
+import pymongo
+
+app=Flask(__name__, static_folder="public", static_url_path="/")
+
+@app.route("/")
+def index():
+    return redirect("main/main.html")
+
