413a5fc458719cec2f12f2b474ce92321da575b6
Your Name <you@example.com>
2022-01-23 22:01:43 +0800

    ver1

--- app.py
+++ app.py
@@ -1,8 +1,160 @@
-from flask import Flask, redirect, session, render_template, request
+from flask import Flask, redirect, session, render_template, request, url_for
 import pymongo
 
+client = pymongo.MongoClient("mongodb+srv://root:root123@realmcluster.rbqar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.website
+collection=db.users
 app=Flask(__name__, static_folder="public", static_url_path="/")
 
-@app.route("/")
+def value_caculate():
+    pass
+
+@app.route("/") # 主頁面
 def index():
     return redirect("/main/main.html")
+
+@app.route("/res", methods=["POST"])
+def res():
+    company=request.form["company"]
+    years=request.form["year"]
+    eps_1=request.form["EPS_1"]
+    eps_2=request.form["EPS_2"]
+    eps_3=request.form["EPS_3"]
+    eps_4=request.form["EPS_4"]
+    m_1=request.form["m_1"]
+    m_2=request.form["m_2"]
+    m_3=request.form["m_3"]
+    m_4=request.form["m_4"]
+    m_5=request.form["m_5"]
+    m_6=request.form["m_6"]
+    m_7=request.form["m_7"]
+    m_8=request.form["m_8"]
+    m_9=request.form["m_9"]
+    m_10=request.form["m_10"]
+    m_11=request.form["m_11"]
+    m_12=request.form["m_12"]
+    dividend_rate=request.form["dividend_rate"]
+    last_update=request.form["date"]
+    share=request.form["share"]
+    if (m_1=="" and m_2==""):
+        eps_1=float(eps_1)
+        eps_2=float(eps_2)
+        eps_3=float(eps_3)
+        eps_4=float(eps_4)
+        eps_year=eps_1+eps_2+eps_3+eps_4
+    else:
+        m_1=float(m_1)
+        m_2=float(m_2)
+        m_3=float(m_3)
+        m_4=float(m_4)
+        m_5=float(m_5)
+        m_6=float(m_6)
+        m_7=float(m_7)
+        m_8=float(m_8)
+        m_9=float(m_9)
+        m_10=float(m_10)
+        m_11=float(m_11)
+        m_12=float(m_12)
+        eps_year=m_1+m_2+m_3+m_4+m_4+m_5+m_6+m_7+m_7+m_8+m_9+m_10+m_11+m_12
+    divend=str(float(dividend_rate)*float(eps_year))
+    yield_=str((float(dividend_rate)*float(eps_year))/float(share))
+    try:
+        datas_get=collection.find()
+        data_get=[]
+        for i in datas_get:
+            data_get.append(i)
+        print(data_get)
+        if data_get[0]==None:
+            collection.insert_one({
+            "company":company,
+            "date":last_update,
+            years:{
+                "EPS_year":eps_year,
+                "EPS_Q1":eps_1,
+                "EPS_Q2":eps_2,
+                "EPS_Q3":eps_3,
+                "EPS_Q4":eps_4,
+                "EPS_m_1":m_1,
+                "EPS_m_2":m_2,
+                "EPS_m_3":m_3,
+                "EPS_m_4":m_4,
+                "EPS_m_5":m_5,
+                "EPS_m_6":m_6,
+                "EPS_m_7":m_7,
+                "EPS_m_8":m_8,
+                "EPS_m_9":m_9,
+                "EPS_m_10":m_10,
+                "EPS_m_11":m_11,
+                "EPS_m_12":m_12,
+                "dividend_rate":dividend_rate, # 配息率
+                "dividend":divend, # 配息
+                "yield":yield_ # 殖利率
+            }
+        })
+        for i in data_get:
+            if i['company']==company:
+                collection.update_one({
+                    "company":company
+                },{
+                    "$set":{
+                        "date":last_update,
+                        years:{
+                            "EPS_year":eps_year,
+                            "EPS_Q1":eps_1,
+                            "EPS_Q2":eps_2,
+                            "EPS_Q3":eps_3,
+                            "EPS_Q4":eps_4,
+                            "EPS_m_1":m_1,
+                            "EPS_m_2":m_2,
+                            "EPS_m_3":m_3,
+                            "EPS_m_4":m_4,
+                            "EPS_m_5":m_5,
+                            "EPS_m_6":m_6,
+                            "EPS_m_7":m_7,
+                            "EPS_m_8":m_8,
+                            "EPS_m_9":m_9,
+                            "EPS_m_10":m_10,
+                            "EPS_m_11":m_11,
+                            "EPS_m_12":m_12,
+                            "dividend_rate":dividend_rate, # 配息率
+                            "dividend":divend, # 配息
+                            "yield":yield_ # 殖利率
+                        }
+                    }
+                })
+    except:
+        collection.insert_one({
+            "company":company,
+            "date":last_update,
+            years:{
+                "EPS_year":eps_year,
+                "EPS_Q1":eps_1,
+                "EPS_Q2":eps_2,
+                "EPS_Q3":eps_3,
+                "EPS_Q4":eps_4,
+                "EPS_m_1":m_1,
+                "EPS_m_2":m_2,
+                "EPS_m_3":m_3,
+                "EPS_m_4":m_4,
+                "EPS_m_5":m_5,
+                "EPS_m_6":m_6,
+                "EPS_m_7":m_7,
+                "EPS_m_8":m_8,
+                "EPS_m_9":m_9,
+                "EPS_m_10":m_10,
+                "EPS_m_11":m_11,
+                "EPS_m_12":m_12,
+                "dividend_rate":dividend_rate, # 配息率
+                "dividend":divend, # 配息
+                "yield":yield_ # 殖利率
+            }
+        })
+
+    return redirect(url_for("display"), code=307)
+
+@app.route("/dis", methods=["POST"])
+def display():
+    return redirect("/display/display.html")
+    
+app.debug=True
+app.run()
---
+++ public/main/js/main.js
@@ -0,0 +1,135 @@
+state=0;
+
+function clicke(){
+    var checked = document.querySelector('[name=EPS_check]:checked');
+    if (checked){
+        state+=1;
+        document.getElementById("EPS_1").style.display="none";
+        document.getElementById("EPS_2").style.display="none";
+        document.getElementById("EPS_3").style.display="none";
+        document.getElementById("EPS_4").style.display="none";
+        document.getElementById("EPS_1").value="";
+        document.getElementById("EPS_2").value="";
+        document.getElementById("EPS_3").value="";
+        document.getElementById("EPS_4").value="";
+        document.getElementById("m_1").style.display="inline";
+        document.getElementById("m_2").style.display="inline";
+        document.getElementById("m_3").style.display="inline";
+        document.getElementById("m_4").style.display="inline";
+        document.getElementById("m_5").style.display="inline";
+        document.getElementById("m_6").style.display="inline";
+        document.getElementById("m_7").style.display="inline";
+        document.getElementById("m_8").style.display="inline";
+        document.getElementById("m_9").style.display="inline";
+        document.getElementById("m_10").style.display="inline";
+        document.getElementById("m_11").style.display="inline";
+        document.getElementById("m_12").style.display="inline";
+    }
+    else{
+        state-=1;
+        document.getElementById("EPS_1").style.display="inline";
+        document.getElementById("EPS_2").style.display="inline";
+        document.getElementById("EPS_3").style.display="inline";
+        document.getElementById("EPS_4").style.display="inline";
+        document.getElementById("m_1").value="";
+        document.getElementById("m_2").value="";
+        document.getElementById("m_3").value="";
+        document.getElementById("m_4").value="";
+        document.getElementById("m_5").value="";
+        document.getElementById("m_6").value="";
+        document.getElementById("m_7").value="";
+        document.getElementById("m_8").value="";
+        document.getElementById("m_9").value="";
+        document.getElementById("m_10").value="";
+        document.getElementById("m_11").value="";
+        document.getElementById("m_12").value="";
+        document.getElementById("m_1").style.display="none";
+        document.getElementById("m_2").style.display="none";
+        document.getElementById("m_3").style.display="none";
+        document.getElementById("m_4").style.display="none";
+        document.getElementById("m_5").style.display="none";
+        document.getElementById("m_6").style.display="none";
+        document.getElementById("m_7").style.display="none";
+        document.getElementById("m_8").style.display="none";
+        document.getElementById("m_9").style.display="none";
+        document.getElementById("m_10").style.display="none";
+        document.getElementById("m_11").style.display="none";
+        document.getElementById("m_12").style.display="none";
+    }
+}
+
+
+function checkm(){
+    var issue=0;
+    var eps_1=document.form.EPS_1.value;
+    var eps_2=document.form.EPS_2.value;
+    var eps_3=document.form.EPS_3.value;
+    var eps_4=document.form.EPS_4.value;
+
+    var m_1=document.form.m_1.value;
+    var m_2=document.form.m_2.value;
+    var m_3=document.form.m_3.value;
+    var m_4=document.form.m_4.value;
+    var m_5=document.form.m_5.value;
+    var m_6=document.form.m_6.value;
+    var m_7=document.form.m_7.value;
+    var m_8=document.form.m_8.value;
+    var m_9=document.form.m_9.value;
+    var m_10=document.form.m_10.value;
+    var m_11=document.form.m_11.value;
+    var m_12=document.form.m_12.value;
+
+    var dividend_rate=document.form.dividend_rate.value;
+    var share=document.form.share.value;
+    // try{
+    //     eps_unit=parseInt(eps);
+    //     if (eps_unit>1000){
+    //         alert("EPS數字過大");
+    //         issue+=1;
+    //     }
+    // }
+    // catch{
+    //     alert("EPS應為數字而非"+typeof(eps));
+    //     eps="";
+    // }
+    if (state==1){
+        if (eps_1.length<=0 | eps_2.length<=0 | eps_3.length<=0 | eps_4.length<=0){
+        issue+=1;
+        alert("EPS不可為空");
+    }
+    else{
+        if (m_1.length<=0 |
+            m_2.length<=0 |
+            m_3.length<=0 |
+            m_4.length<=0 |
+            m_5.length<=0 |
+            m_6.length<=0 |
+            m_7.length<=0 |
+            m_8.length<=0 |
+            m_9.length<=0 |
+            m_10.length<=0 |
+            m_11.length<=0 |
+            m_12.length<=0
+            ){
+                issue+=1;
+                alert("EPS不可為空");
+            }
+    }
+    }
+    if (dividend_rate.length<=0){
+        issue+=1;
+        alert("配息率不可為空");
+    }
+    if (share.length<=0){
+        issue+=1;
+        alert("股價不可為空");
+    }
+    console.log(share.length);
+    if (issue==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+    return false
+}
